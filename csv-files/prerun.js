6957a79ed4633eba3e4575a5115dd4184174bf0f
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2024-06-09 23:29:06 +0530

    Add files via upload

---
+++ csv-files/prerun.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema, mongo } from "mongoose"
+import { parse } from "node-html-parser"
+import { existsSync, mkdirSync, createReadStream, readFileSync } from "fs"
+import { join } from "path"
+import dotenv from "dotenv"
+import Grid from "gridfs-stream"
+import mongodb from "mongodb"
+
+dotenv.config()
+
+const fileNamesSchema = new Schema({
+	id: Schema.ObjectId,
+	name: String,
+	date: Date,
+	url: String,
+})
+
+const FileName = mongoose.model("fileName", fileNamesSchema)
+
+const fileScehma = new Schema({
+	id: Schema.ObjectId,
+	name: {
+		type: String,
+		unique: true,
+	},
+	data: String,
+	date: Date,
+})
+
+const File = mongoose.model("files", fileScehma)
+
+export default async function prerun() {
+	const fileName = "2024-06-03"
+	const fileNamePath = join(process.cwd(), "csv-files", `${fileName}.csv`)
+
+	await mongoose.connect(process.env.MONGO_CONNECTION)
+	console.log("Connection Success:", mongoose.connection.name)
+
+	const fileContents = readFileSync(fileNamePath)
+	console.log("Reading File: ", fileName)
+
+	const newFileRecord = new File()
+	newFileRecord.name = fileName
+	newFileRecord.date = new Date()
+	newFileRecord.data = fileContents
+	await newFileRecord.save()
+
+	console.log("File Write Success:", `${fileName}.csv`)
+	console.log("File: ", newFileRecord)
+}
+
+prerun()
+	.then((res) => {
+		console.log(res)
+		mongoose.disconnect()
+	})
+	.catch((e) => {
+		console.log("Error:", e)
+		mongoose.disconnect()
+	})
---
+++ dev.js
@@ -0,0 +1,331 @@
+import { parse } from "node-html-parser"
+import { join } from "path"
+import { readFileSync, existsSync, mkdirSync, writeFileSync } from "fs"
+import { parse as CSVParser } from "csv-parse/sync"
+import dotenv from "dotenv"
+import mongoose, { Schema, mongo } from "mongoose"
+
+dotenv.config()
+
+function getFormattedDate(d = new Date().toString()) {
+	const date = new Date(d)
+	const year = date.getFullYear()
+	const month = String(date.getMonth() + 1).padStart(2, "0")
+	const day = String(date.getDate()).padStart(2, "0")
+
+	return `${year}-${month}-${day}`
+}
+
+const fileNamesSchema = new Schema({
+	id: Schema.ObjectId,
+	name: String,
+	date: Date,
+	url: String,
+})
+
+const FileName = mongoose.model("fileName", fileNamesSchema)
+
+const fileScehma = new Schema({
+	id: Schema.ObjectId,
+	name: {
+		type: String,
+		unique: true,
+	},
+	data: String,
+	date: Date,
+})
+
+const File = mongoose.model("files", fileScehma)
+
+// creating a mongo schema for today's csv data
+const additionSchema = new Schema({
+	id: Schema.ObjectId,
+	name: String,
+	townCity: String,
+	county: String,
+	type: String,
+	route: String,
+	date: String,
+})
+
+// assigning a model to mongodb user userSchema
+const Addition = mongoose.model("addition", additionSchema)
+
+// creating a mongo schema for today's csv data
+const updateSchema = new Schema({
+	id: Schema.ObjectId,
+	name: String,
+	townCity: String,
+	county: String,
+	type: String,
+	route: String,
+	date: String,
+})
+
+// assigning a model to mongodb user userSchema
+const Updates = mongoose.model("updates", updateSchema)
+
+// creating a mongo schema for today's csv data
+const removalSchema = new Schema({
+	id: Schema.ObjectId,
+	name: String,
+	townCity: String,
+	county: String,
+	type: String,
+	route: String,
+	date: String,
+})
+
+// assigning a model to mongodb user userSchema
+const Removal = mongoose.model("removals", removalSchema)
+
+export default async function handler() {
+	const CSVFileDir = join(process.cwd(), "csv-files")
+
+	if (!existsSync(CSVFileDir)) {
+		mkdirSync(CSVFileDir)
+	}
+
+	await mongoose.connect(process.env.MONGO_CONNECTION)
+	console.log("Connection Success:", mongoose.connection.name)
+
+	const csvFileRes = await fetch(
+		"https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers",
+		{
+			headers: {
+				"Content-Type": "text/html",
+			},
+		}
+	)
+
+	if (!csvFileRes.ok) {
+		console.log("Current update file link fetch failed!")
+		return
+	}
+	const govUkCSVHtml = parse(await csvFileRes.text())
+
+	const link = govUkCSVHtml.querySelector(".govuk-link.gem-c-attachment__link")
+	const lastUpdateDate = govUkCSVHtml.querySelectorAll(
+		".app-c-published-dates__change-item time"
+	)[1].innerText
+
+	// Sample Link
+	// https://assets.publishing.service.gov.uk/media/6659951fd470e3279dd33443/2024-05-31_-_Worker_and_Temporary_Worker.csv
+	// https://assets.publishing.service.gov.uk/media/665ed5eadc15efdddf1a86b6/2024-06-04_-_Worker_and_Temporary_Worker.csv
+	const fileLink = link.getAttribute("href")
+	const fileLinkArr = fileLink.replace("/preview", "").split("/")
+	const fileName = fileLinkArr[fileLinkArr.length - 1]
+
+	const currentUpdateFileLink = fileLink
+	const currentUpdateFileDate = fileName.split("_-_")[0]
+	const lastUpdateFileDate = getFormattedDate(lastUpdateDate)
+
+	console.log("LINK: ", fileLink)
+	console.log("FILENAME: ", fileName)
+	console.log("LAST_UPDATE_DATE: ", lastUpdateDate)
+	console.log("LAST_UPDATE_DATE_FORMATTED: ", getFormattedDate(lastUpdateDate))
+
+	const fileNameRecord = new FileName()
+	fileNameRecord.name = currentUpdateFileDate
+	fileNameRecord.date = new Date()
+	fileNameRecord.url = fileLink
+	await fileNameRecord.save()
+	console.log("fileNameRecord", fileNameRecord)
+
+	const thisDaysFileNameCSV = `${currentUpdateFileDate}.csv`
+	const prevDaysFileNameCSV = `${lastUpdateFileDate}.csv`
+	console.log({ prevDaysFileNameCSV, thisDaysFileNameCSV })
+
+	console.log(`Got Link: ${currentUpdateFileLink}`)
+	console.log(`Today's Date: ${currentUpdateFileDate}`)
+
+	console.log("Fetching File!")
+	const res = await fetch(currentUpdateFileLink)
+
+	if (!res.ok) {
+		console.log("File Write Failed, Try Again!")
+		return
+	}
+
+	const currentUpdateCSVFile = await res.text()
+	console.log("Downloded CSV File!")
+
+	const newFileCheck = await File.findOne({
+		name: currentUpdateFileDate,
+	})
+		.select("name")
+		.exec()
+	if (newFileCheck?.name === currentUpdateFileDate) {
+		console.log(`
+File with name ${currentUpdateFileDate} already saved to MongoDB
+This could also mean that there is no update to CSV Source file 
+since last script run.
+Exiting Script!
+`)
+		return
+	} else {
+		const newFileRecord = new File()
+		newFileRecord.name = currentUpdateFileDate
+		newFileRecord.date = new Date()
+		newFileRecord.data = currentUpdateCSVFile
+		await newFileRecord.save()
+
+		console.log("File saved to MongoDB!:", `${currentUpdateFileDate}.csv`)
+		console.log("File: ", newFileRecord)
+	}
+
+	const currentUpdateCSVString = currentUpdateCSVFile
+
+	const lastUpdateFile = await File.findOne({
+		name: lastUpdateFileDate,
+	}).exec()
+	console.log("File Downloded!: ", lastUpdateFile.name)
+
+	if (!lastUpdateFile) {
+		console.log("Please add last updates file to MongoDB first, use prernu.js!")
+		return
+	}
+
+	const lastUpdateCSVString = lastUpdateFile.data
+
+	const thisDateCSVDataParsed = CSVParser(currentUpdateCSVString, {
+		columns: true,
+	})
+	const prevDateCSVDataParsed = CSVParser(lastUpdateCSVString, {
+		columns: true,
+	})
+
+	console.log("thisDateCSVDataParsed", thisDateCSVDataParsed.length)
+	console.log("prevDateCSVDataParsed", prevDateCSVDataParsed.length)
+
+	const prevDayOrgObjectsByName = {}
+	const thisDayOrgObjectsByName = {}
+	const orgNameCol = "Organisation Name"
+
+	prevDateCSVDataParsed.forEach((obj) => {
+		prevDayOrgObjectsByName[obj[orgNameCol]] = obj
+	})
+	thisDateCSVDataParsed.forEach((obj) => {
+		thisDayOrgObjectsByName[obj[orgNameCol]] = obj
+	})
+
+	console.log({
+		prevDayOrgObjectsByName: Object.keys(prevDayOrgObjectsByName).length,
+		thisDayOrgObjectsByName: Object.keys(thisDayOrgObjectsByName).length,
+	})
+
+	let prevDateNames = new Set()
+	let thisDateNames = new Set()
+
+	thisDateCSVDataParsed.map((row) => {
+		if (row[orgNameCol]) {
+			thisDateNames.add(row[orgNameCol])
+		}
+	})
+	prevDateCSVDataParsed.map((row) => {
+		if (row[orgNameCol]) {
+			prevDateNames.add(row[orgNameCol])
+		}
+	})
+
+	console.log({
+		prevDateColNames: prevDateNames.size,
+		thisDateColNames: thisDateNames.size,
+	})
+
+	const addedOrgNames = [...thisDateNames].filter((x) => !prevDateNames.has(x))
+	const removedOrgNames = [...prevDateNames].filter(
+		(x) => !thisDateNames.has(x)
+	)
+
+	console.log(
+		`Values in ${lastUpdateFileDate} but not in ${currentUpdateFileDate}:`,
+		removedOrgNames.length
+	)
+	console.log(
+		`Values in ${currentUpdateFileDate} but not in ${lastUpdateFileDate}:`,
+		addedOrgNames.length
+	)
+
+	const prevDateFullObjs = {}
+	const thisDateFullObjs = {}
+
+	addedOrgNames.forEach((orgName) => {
+		thisDateFullObjs[orgName] = thisDayOrgObjectsByName[orgName]
+	})
+	removedOrgNames.forEach((orgName) => {
+		prevDateFullObjs[orgName] = prevDayOrgObjectsByName[orgName]
+	})
+
+	console.log({
+		prevDateFullObjs: Object.keys(prevDateFullObjs).length,
+		thisDateFullObjs: Object.keys(thisDateFullObjs).length,
+	})
+
+	addedOrgNames.map(async (row) => {
+		const colNames = Object.keys(thisDateFullObjs[row])
+		const record = new Addition()
+		record.name = thisDateFullObjs[row][colNames[0]]
+		record.townCity = thisDateFullObjs[row][colNames[1]]
+		record.county = thisDateFullObjs[row][colNames[2]]
+		record.type = thisDateFullObjs[row][colNames[3]]
+		record.route = thisDateFullObjs[row][colNames[4]]
+		record.date = currentUpdateFileDate
+		await record.save()
+	})
+	console.log("All Additions Added to DB!")
+
+	removedOrgNames.map(async (row) => {
+		const colNames = Object.keys(prevDateFullObjs[row])
+		const record = new Removal()
+		record.name = prevDateFullObjs[row][colNames[0]]
+		record.townCity = prevDateFullObjs[row][colNames[1]]
+		record.county = prevDateFullObjs[row][colNames[2]]
+		record.type = prevDateFullObjs[row][colNames[3]]
+		record.route = prevDateFullObjs[row][colNames[4]]
+		record.date = currentUpdateFileDate
+		await record.save()
+	})
+	console.log("All Removals Added to DB!")
+
+	const updatedOrgsObjs = []
+	const colToCheckForUpdates = "Town/City"
+
+	Object.keys(thisDayOrgObjectsByName).forEach((orgName) => {
+		const prevDayObj = prevDayOrgObjectsByName[orgName]
+		if (!prevDayObj) {
+			return
+		}
+
+		const thisDayObj = thisDayOrgObjectsByName[orgName]
+
+		// console.log("Checking: ", orgName)
+		if (prevDayObj[colToCheckForUpdates] !== thisDayObj[colToCheckForUpdates]) {
+			updatedOrgsObjs.push(thisDayObj)
+		}
+	})
+	console.log("All Checked For Updates!")
+
+	updatedOrgsObjs.forEach(async (update) => {
+		const colNames = Object.keys(update)
+		const record = new Updates()
+		record.name = update[colNames[0]]
+		record.townCity = update[colNames[1]]
+		record.county = update[colNames[2]]
+		record.type = update[colNames[3]]
+		record.route = update[colNames[4]]
+		record.date = currentUpdateFileDate
+		await record.save()
+	})
+	console.log("All Updates Added to DB!")
+}
+
+handler()
+	.then((res) => {
+		console.log(res)
+		// mongoose.disconnect()
+	})
+	.catch((e) => {
+		console.log("Error:", e)
+		mongoose.disconnect()
+	})
