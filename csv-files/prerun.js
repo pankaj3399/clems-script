8ae2658c349f053414f7a4dc2e12e5baac23c475
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2025-03-06 23:50:16 +0530

    Push Release

--- csv-files/prerun.js
+++ csv-files/prerun.js
@@ -1,60 +1,60 @@
-import mongoose, { Schema, mongo } from "mongoose"
-import { parse } from "node-html-parser"
-import { existsSync, mkdirSync, createReadStream, readFileSync } from "fs"
-import { join } from "path"
-import dotenv from "dotenv"
-import Grid from "gridfs-stream"
-import mongodb from "mongodb"
+import mongoose, { Schema, mongo } from "mongoose";
+import { parse } from "node-html-parser";
+import { existsSync, mkdirSync, createReadStream, readFileSync } from "fs";
+import { join } from "path";
+import dotenv from "dotenv";
+import Grid from "gridfs-stream";
+import mongodb from "mongodb";
 
-dotenv.config()
+dotenv.config();
 
 const fileNamesSchema = new Schema({
-	id: Schema.ObjectId,
-	name: String,
-	date: Date,
-	url: String,
-})
+  id: Schema.ObjectId,
+  name: String,
+  date: Date,
+  url: String,
+});
 
-const FileName = mongoose.model("fileName", fileNamesSchema)
+const FileName = mongoose.model("fileName", fileNamesSchema);
 
 const fileScehma = new Schema({
-	id: Schema.ObjectId,
-	name: {
-		type: String,
-		unique: true,
-	},
-	data: String,
-	date: Date,
-})
+  id: Schema.ObjectId,
+  name: {
+    type: String,
+    unique: true,
+  },
+  data: String,
+  date: Date,
+});
 
-const File = mongoose.model("files", fileScehma)
+const File = mongoose.model("files", fileScehma);
 
 export default async function prerun() {
-	const fileName = "2024-06-10"
-	const fileNamePath = join(process.cwd(), "csv-files", `${fileName}.csv`)
+  const fileName = "2025-03-05";
+  const fileNamePath = join(process.cwd(), "csv-files", `${fileName}.csv`);
 
-	await mongoose.connect(process.env.MONGO_CONNECTION)
-	console.log("Connection Success:", mongoose.connection.name)
+  await mongoose.connect(process.env.MONGO_CONNECTION);
+  console.log("Connection Success:", mongoose.connection.name);
 
-	const fileContents = readFileSync(fileNamePath)
-	console.log("Reading File: ", fileName)
+  const fileContents = readFileSync(fileNamePath);
+  console.log("Reading File: ", fileName);
 
-	const newFileRecord = new File()
-	newFileRecord.name = fileName
-	newFileRecord.date = new Date()
-	newFileRecord.data = fileContents
-	await newFileRecord.save()
+  const newFileRecord = new File();
+  newFileRecord.name = fileName;
+  newFileRecord.date = new Date();
+  newFileRecord.data = fileContents;
+  await newFileRecord.save();
 
-	console.log("File Write Success:", `${fileName}.csv`)
-	console.log("File: ", newFileRecord)
+  console.log("File Write Success:", `${fileName}.csv`);
+  console.log("File: ", newFileRecord);
 }
 
 prerun()
-	.then((res) => {
-		console.log(res)
-		mongoose.disconnect()
-	})
-	.catch((e) => {
-		console.log("Error:", e)
-		mongoose.disconnect()
-	})
+  .then((res) => {
+    console.log(res);
+    mongoose.disconnect();
+  })
+  .catch((e) => {
+    console.log("Error:", e);
+    mongoose.disconnect();
+  });
